9c282fbe0c1a9db634746afaaa4318db9bf7873d
Nishantagrawal2303 <nishant2303agarwal@gmail.com>
2025-08-27 17:07:35 +0530

    Add files via upload

---
+++ class4/Script.js
@@ -0,0 +1,95 @@
+
+// // array  is created
+
+// let arr=[10,20,30,40,50,60];
+
+// console.log(arr);
+
+// // the pop method
+// arr.pop(2);
+// console.log(arr);
+
+
+// // bhai push mtlb array ke last me push krna
+// arr.push(90)
+// console.log(arr);
+
+
+// // unshift mtlb start me add krna hota he
+
+// arr.unshift(100);
+// console.log(arr);
+
+// // shift mtlb start se udaa dena      
+
+// arr.shift(100);
+// console.log(arr);
+
+// // reverse an array
+
+// arr.reverse();
+// console.log(arr);
+
+
+
+// // array destructuring 
+
+// let arrr=[1,2,3,4,5];
+
+// console.log(arrr);
+
+// let[first,second]=arrr;
+
+// console.log(first);
+// console.log(second);
+
+
+
+
+// // object destructuring 
+
+// let obj={
+//     fname : "nishant",
+//     age:25,
+//     education:"B.tech",
+//     data:{
+//         city:"sanawad",
+//         nish :[21,22,23]
+//     }
+  
+
+// }
+// // console.log(obj);
+
+// obj.city= "indore"
+// console.log(obj);
+
+// let {fname,age}=obj;
+
+// let {data}=obj;
+// let{city,nish}=data;
+
+// console.log(city);   // yaha apn ne objet me se city ki value nikal li bde
+// console.log(nish[2]);   // yaha array me se value nikal liii bde 
+
+
+// // Rest operator
+
+
+// deep copy     isme original me nhi hota he change
+
+// shallow copy     isme orignial me bhi change hoga he
+
+
+// array is for homework 
+
+
+
+
+
+
+
+
+
+
+

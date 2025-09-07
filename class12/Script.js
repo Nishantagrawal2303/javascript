58c90f34bce7cb3082305f19f8fa53077ac49a30
Nishantagrawal2303 <nishant2303agarwal@gmail.com>
2025-09-07 17:39:14 +0530

    Add files via upload

---
+++ class12/Script.js
@@ -0,0 +1,20 @@
+
+let addtaskbtn = document.getElementById("addtaskbtn");
+function addfunction() {
+  let newtask = document.getElementsByTagName("ul");
+  let newli = document.createElement("li");
+  newli.setAttribute("class", "newli")
+  button = document.createElement("button");
+  button.innerText = "x";
+  let newtext = document.getElementById("yyy").value;
+  newli.append(newtext);
+  newli.append(button);
+  newtask[0].append(newli);
+  document.getElementById("yyy").value = "";
+  //remove krne ka code
+  button.addEventListener("click", () => {
+    newli.remove();
+  });
+}
+addtaskbtn.addEventListener("click", addfunction);
+
